a46ac475e21c0ab3fc3a905095449cc9607dd9d0
Ishmatu Aulia <ishmatuaulia@gmail.com>
2025-10-23 22:36:16 +0700

    Add files via upload

---
+++ CODE/build_script_build-91dff510b5d3caf1.d
@@ -0,0 +1,5 @@
+/home/riska-laena/Documents/tubes/project-tubes/target/release/build/heapless-91dff510b5d3caf1/build_script_build-91dff510b5d3caf1.d: /home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/heapless-0.8.0/build.rs
+
+/home/riska-laena/Documents/tubes/project-tubes/target/release/build/heapless-91dff510b5d3caf1/build_script_build-91dff510b5d3caf1: /home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/heapless-0.8.0/build.rs
+
+/home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/heapless-0.8.0/build.rs:
