1076047c701917bb4dcf61b98575fe4db297407c
Ishmatu Aulia <ishmatuaulia@gmail.com>
2025-10-23 22:38:24 +0700

    Add files via upload

---
+++ CODE/build_script_build-358cbc9f6ae53f50.d
@@ -0,0 +1,5 @@
+/home/riska-laena/Documents/tubes/project-tubes/target/release/build/nix-358cbc9f6ae53f50/build_script_build-358cbc9f6ae53f50.d: /home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.29.0/build.rs
+
+/home/riska-laena/Documents/tubes/project-tubes/target/release/build/nix-358cbc9f6ae53f50/build_script_build-358cbc9f6ae53f50: /home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.29.0/build.rs
+
+/home/riska-laena/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/nix-0.29.0/build.rs:
